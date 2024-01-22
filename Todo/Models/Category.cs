5916cb013a01f326f896f07679ff342db7fe3ef1
rlroudayna <louatami.roudayna.2002@gmail.com>
2024-01-22 17:05:41 +0100

    Ajoutez des fichiers projet.

---
+++ Todo/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoDemo.Models
+{
+    public class Category
+    {
+        public string CategoryId { get; set; } = string.Empty;
+
+        public string Name { get; set; }= string.Empty;
+      
+    }
+}
